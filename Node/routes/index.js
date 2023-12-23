86b64d9fcdf4dff8b812c9ff3cf8f6e5c392416d
howardchou <howard9107@gmail.com>
2023-12-23 22:40:20 +0800

    v2.0
    Upload AR unity file and make the anim

--- Node/routes/index.js
+++ Node/routes/index.js
@@ -12,22 +12,16 @@ router.get('/play', function(req, res, next) {
   res.redirect('/play/unity-game');
 });
 
-// Serve Unity WebGL game files
-router.use('/artest1/usear', express.static('public/AR'));
-router.get('/artest1', function(req, res, next) {
-  // res.redirect('/artest1/usear');
-  res.redirect('https://howardchou.github.io/test1217_1/');
-});
 
-router.use('/artest2/usear', express.static('public/ARtest'));
-router.get('/artest2', function(req, res, next) {
-  // res.redirect('/artest2/usear');
+router.use('/artest1/usear', express.static('public/ARtest'));
+router.get('/artest1', function(req, res, next) {
+  res.redirect('/artest1/usear');
 });
 
 
 // Serve Unity WebGL game files
 router.use('/simpson/usear', express.static('public/Simpson'));
-router.get('/simpson1', function(req, res, next) {
+router.get('/simpson', function(req, res, next) {
   // res.redirect('/simpson/usear');
   res.redirect('https://kevinlin1120.github.io/Simpson-s_Skate/');
 });
