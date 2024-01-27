688162e674852fb2c7aef459853a30818533e744
howardchou <howard9107@gmail.com>
2024-01-27 22:52:02 +0800

    ranking page and 之間的連接

--- Howard's-test/WEB_Test/JS/badge.js
+++ Howard's-test/WEB_Test/JS/badge.js
@@ -17,6 +17,25 @@ function showImage(buttonId) {
     };
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+    var menuBar = document.querySelector('.menu-bar');
+    var rankWindowBtn = document.getElementById('openRankWindowBtn');
+    var weekWindowBtn = document.getElementById('openWeekWindowBtn');
+    var toggleMenuBtn = document.getElementById('toggleMenuBtn');
+
+    toggleMenuBtn.addEventListener('click', function () {
+        if (menuBar.style.display === 'none' || menuBar.style.display === '') {
+            menuBar.style.display = 'block';
+            rankWindowBtn.style.display = 'none';
+            weekWindowBtn.style.display = 'none';
+        } else {
+            menuBar.style.display = 'none';
+            rankWindowBtn.style.display = 'block';  // Adjust this to 'block' or 'inline-block' as needed
+            weekWindowBtn.style.display = 'block';  // Adjust this to 'block' or 'inline-block' as needed
+        }
+    });
+});
+
 function scrollToSection(sectionId) {
     var targetElement = document.getElementById(sectionId);
 
@@ -43,15 +62,18 @@ function closeFloatingWindow() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    const openButton = document.getElementById("openButton");
+    const openWeekButton = document.getElementById("openWeekWindowBtn");
     const floatingWindow = document.getElementById("WeekWindow");
-    const closeButton = document.getElementById("closeButton");
+    const closeWeekButton = document.getElementById("closeWeekWindowBtn");
+    const body = document.body;
 
-    openButton.addEventListener("click", function () {
+    openWeekButton.addEventListener("click", function () {
         floatingWindow.style.display = "block";
+        body.style.overflow = "hidden"; // Disable scrolling on the body
     });
 
-    closeButton.addEventListener("click", function () {
+    closeWeekButton.addEventListener("click", function () {
         floatingWindow.style.display = "none";
+        body.style.overflow = ""; // Enable scrolling on the body
     });
 });
