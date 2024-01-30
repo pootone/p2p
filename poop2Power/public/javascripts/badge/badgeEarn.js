1b8a2f22f45663a3ca52b07f4782a8c6a57ba736
howardchou <howard9107@gmail.com>
2024-01-31 02:38:32 +0800

    upload badge

--- poop2Power/public/javascripts/badge/badgeEarn.js
+++ poop2Power/public/javascripts/badge/badgeEarn.js
@@ -4,10 +4,10 @@ var currentUrl = window.location.href;
 // 检查URL是否包含?isFirst=true
 if (currentUrl.indexOf('?isFirst=true') !== -1) {
     // 如果是第一次访问，直接修改HTML内容
-    document.querySelector('.image-container img').src = 'Assests/images/徽章/badge/badge1.png';
+    document.querySelector('.image-container img').src = '../images/badge/徽章/badge/badge1.png';
     // 添加第二个图片和按钮
     var addButton = document.createElement('img');
-    addButton.src = 'Assests/images/徽章/加.png';
+    addButton.src = '../images/badge/徽章/加.png';
     addButton.id = 'button-badge1';
     addButton.className = 'addBtn';
     addButton.onclick = function() {
--- poop2Power/routes/users.js
+++ poop2Power/routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+  // const filePath = __dirname + '/../public/HTML/badge.html';
+  // res.sendFile(filePath);
 
+  res.sendFile(path.join(__dirname, 'HTML/badge.html'));
+});
 module.exports = router;
