ef39515d0fe44286c1370464eedefeb969fdb6c1
howardchou <howard9107@gmail.com>
2024-01-31 10:42:27 +0800

    fixed ranking

--- poop2Power/routes/users.js
+++ poop2Power/routes/users.js
@@ -9,4 +9,11 @@ router.get('/', function(req, res, next) {
 
   res.sendFile(path.join(__dirname, 'HTML/badge.html'));
 });
+
+router.get('/ranking', function(req, res, next) {
+  // const filePath = __dirname + '/../public/HTML/badge.html';
+  // res.sendFile(filePath);
+
+  res.sendFile(path.join(__dirname, 'HTML/ranking.html'));
+});
 module.exports = router;
