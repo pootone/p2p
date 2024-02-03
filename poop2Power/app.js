19f8223c7882218732ce18b83a06042e9cf6a449
howardchou <howard9107@gmail.com>
2024-02-03 21:34:34 +0800

    aboutUS done

--- poop2Power/app.js
+++ poop2Power/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var aiCamRouter = require('./routes/aiCam');
 var usersRouter = require('./routes/users');
+var aboutUsRouter = require('./routes/aboutUs');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/aicam', aiCamRouter);
 app.use('/users', usersRouter);
+app.use('/aboutUs', aboutUsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
