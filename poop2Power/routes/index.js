1c3200c52199b75879ecdc7123e76e1b5db0ef7c
howardchou <howard9107@gmail.com>
2024-02-04 22:23:04 +0800

    update

--- poop2Power/routes/index.js
+++ poop2Power/routes/index.js
@@ -4,11 +4,11 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, 'HTML/indexS1.html'));
+  res.sendFile(path.join(__dirname, 'html/index.html'));
 });
 
 router.get('/trans', function(req, res, next) {
-  res.sendFile(path.join(__dirname, 'HTML/indexTrans.html'));
+  res.sendFile(path.join(__dirname, 'html/indexTrans.html'));
 });
 
 module.exports = router;
