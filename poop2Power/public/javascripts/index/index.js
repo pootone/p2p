7abae8c201cde4bfd2a6f15e8641f71febfafd8e
howardchou <howard9107@gmail.com>
2024-02-22 16:16:32 +0800

    badge update

--- poop2Power/public/javascripts/index/index.js
+++ poop2Power/public/javascripts/index/index.js
@@ -93,6 +93,11 @@ function updateCircleImages() {
 
 function showTooltip(tipId) {
     setTimeout(function () {
+        document.getElementById("tipIndex").style.display = 'none';
+        document.getElementById("tipBadge").style.display = 'none';
+        document.getElementById("tipCam").style.display = 'none';
+        document.getElementById("tipAnim").style.display = 'none';
+        document.getElementById("tipAbout").style.display = 'none';
         document.getElementById(tipId).style.display = 'block';
         // Restart the carousel when the tooltip is shown
         badgeCarousel.cycle();
@@ -100,9 +105,11 @@ function showTooltip(tipId) {
 }
 
 function hideTooltip() {
-    document.getElementById("tipCam").style.display = 'none';
+    document.getElementById("tipIndex").style.display = 'none';
     document.getElementById("tipBadge").style.display = 'none';
+    document.getElementById("tipCam").style.display = 'none';
     document.getElementById("tipAnim").style.display = 'none';
+    document.getElementById("tipAbout").style.display = 'none';
     // Pause the carousel when hiding the tooltip
     badgeCarousel.pause();
 }
