6bc681a13a04bf4078479f9357500f69fbd21eae
KevinLin1120 <80439584+KevinLin1120@users.noreply.github.com>
2024-03-24 02:07:05 +0800

    Add user login

--- poop2Power/public/javascripts/badge/badge.js
+++ poop2Power/public/javascripts/badge/badge.js
@@ -1,3 +1,71 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBuGBwRToBzDRfahNTopteuS9fOmEg1so8",
+    authDomain: "no2-2024.firebaseapp.com",
+    projectId: "no2-2024",
+    storageBucket: "no2-2024.appspot.com",
+    messagingSenderId: "350892065045",
+    appId: "1:350892065045:web:83e08ce22324bac4e8291c",
+    measurementId: "G-N9JH7N740F"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+// FirebaseUI config.
+var uiConfig = {
+    signInSuccessUrl: "./badge.html",
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        {
+            provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            buttonColor: '#777E94',
+            iconUrl: '../images/badge/loginModal/google_icon.svg'
+        },
+        {
+            provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            buttonColor: '#D1A4A4',
+            iconUrl: '../images/badge/loginModal/mail_icon.svg'
+        },
+        {
+            provider: firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
+            buttonColor: '#C6B896',
+            iconUrl: '../images/badge/loginModal/anom_icon.svg'
+        }
+    ],
+    signInFlow: "popup",
+};
+
+initApp = function () {
+    firebase.auth().onAuthStateChanged(
+        function (user) {
+            // User is signed in.
+            if (user) {
+                $("#loginModal").hide();
+                $("#title").text(user.displayName + "'s Dashboard");
+            } else {
+                // User is signed out.
+                $("#loginModal").show();
+                $("#title").text("Personal Dashboard");
+            }
+        },
+        function (error) {
+            console.log(error);
+        }
+    );
+}
+
+$().ready(function () {
+    initApp();
+    // Initialize the FirebaseUI Widget using Firebase.
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // The start method will wait until the DOM is loaded.
+    ui.start("#firebaseui-auth-container", uiConfig);
+
+
+    $("#login_skip").click(function () {
+        $("#loginModal").hide();
+    });
+});
+
 function showImage(buttonId) {
     console.log("Button ID: " + buttonId);
 
