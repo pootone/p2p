98d3d2f4612dd2cabbb884e13938600eb943f083
howardchou <howard9107@gmail.com>
2024-04-16 01:28:55 +0800

    update

--- poop2Power/public/javascripts/cornerBTN.js
+++ poop2Power/public/javascripts/cornerBTN.js
@@ -29,12 +29,10 @@ document.addEventListener('DOMContentLoaded', function () {
         if (menuBar.style.display === 'none' || menuBar.style.display === '') {
             menuBar.style.display = 'block';
             rankWindowBtn.style.display = 'none';
-            weekWindowBtn.style.display = 'none';
         } else {
             menuBar.style.display = 'none';
             if (window.innerWidth <= 768) {
                 rankWindowBtn.style.display = 'none';  
-                weekWindowBtn.style.display = 'none';  
             } else {
                 rankWindowBtn.style.display = 'block';  
                 weekWindowBtn.style.display = 'block';  
